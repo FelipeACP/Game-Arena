a9e31c363b5aa46e1962dbbff6e8cef383474c6c
Felipe Pegado <33338930+FelipeACP@users.noreply.github.com>
2020-06-21 13:41:48 -0300

    Custom NavButtons

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,45 +1,58 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 export const Logo = () => (
   <div id="logo">
     <div id="mainnavigation">
-      <NavegationButton
-        key="startpage"
-        link="/"
-        source="https://web.archive.org/web/20191003140654im_/https://naruto-arena.net/images/content/40/navigation.gif"
-      />
-      <NavegationButton
-        key="game"
-        link="/game"
-        source="https://web.archive.org/web/20191003140654im_/https://naruto-arena.net/images/content/41/navigation.gif"
-      />
-      <NavegationButton
-        key="game-manual"
-        link="/game-manual"
-        source="https://web.archive.org/web/20200411191449im_/https://naruto-arena.net/images/content/28/navigation.gif"
-      />
-      <NavegationButton
-        key="ladders"
-        link="/ladders"
-        source="https://web.archive.org/web/20191003140654im_/https://naruto-arena.net/images/content/39/navigation.gif"
-      />
-      <NavegationButton
-        key="mission"
-        link="/ninja-missions"
-        source="https://web.archive.org/web/20191003140654im_/https://naruto-arena.net/images/content/45/navigation.gif"
-      />
-      <NavegationButton
-        key="discord"
-        link="https://discordapp.com/invite/yEFaw6f"
-        source="https://web.archive.org/web/20191003140654im_/https://naruto-arena.net/images/content/61/navigation.jpg"
-      />
+      <ul>
+        <NavegationButton key="startpage" link="/" text="Start Page" />
+        <NavegationButton key="game" link="/game" text="Start Playing" />
+        <NavegationButton
+          key="game-manual"
+          link="/game-manual"
+          text="Game Manual"
+        />
+        <NavegationButton key="ladders" link="/ladders" text="Ladders" />
+        <NavegationButton
+          key="mission"
+          link="/ninja-missions"
+          text="Ninja Missions"
+        />
+        <NavegationButton
+          key="discord"
+          link="https://discordapp.com/invite/yEFaw6f"
+          text="Discord"
+        />
+      </ul>
     </div>
   </div>
 );
 
-const NavegationButton = ({ link, source }) => (
-  <Link to={link}>
-    <img src={source} alt="" />
-  </Link>
+const NavegationButton = ({ link, source, text }) => (
+  <RawButton>
+    <Link to={link}>{text}</Link>
+  </RawButton>
 );
+
+const RawButton = styled.li`
+  position: relative;
+  border-radius: 5px;
+  background-color: #fff;
+  height: 25px;
+  border: 1px solid #000;
+  width: 190px;
+  float: left;
+  margin: 8px auto 0;
+  padding: 4px 0 0 58px;
+  color: black;
+  text-shadow: 0px 0px #000;
+  font-family: "Francois One";
+  font-size: 18px;
+  line-height: 15px;
+  font-weight: 500;
+  font-style: normal;
+  text-transform: none;
+  font-variant: small-caps;
+  letter-spacing: 0;
+`;
